917850f31d366cf18d747de5961c2c3c2fa12105
hiko-ogeqq <knhk0419@gmail.com>
2020-12-21 14:42:01 +0900

    5-2

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+public class User {
+  @Id
+  @Column
+  private String username;
+  @Column
+  private String encodedPassword;
+}
