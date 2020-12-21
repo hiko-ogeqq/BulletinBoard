77a6ed90be5aaea2e75dc2a7aaa24afe5ac5e53b
hiko-ogeqq <knhk0419@gmail.com>
2020-12-21 13:47:34 +0900

    fix indention

--- src/main/java/com/example/demo/BbsRepository.java
+++ src/main/java/com/example/demo/BbsRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface BbsRepository extends JpaRepository<BulletinBoard, Long>{
 
   public BulletinBoard findById(int id);
-	public void deleteById(int id);
+  public void deleteById(int id);
 }
